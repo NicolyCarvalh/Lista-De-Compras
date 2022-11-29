d9b66b93711f61e69faaff3b9c33e98b24d93b03
nicolycarvalh <nicolycarvalh15@gmail.com>
2022-11-29 08:16:59 -0300

    first commit

--- src/Components/Produtos.tsx
+++ src/Components/Produtos.tsx
@@ -2,5 +2,9 @@ import { Link } from "react-router-dom"
 import "./Styles/Produtos.css"
 
 export default function (){
-    
+    return(
+        <body>
+            <Link to={"/Inicial"}><p className="inicial">--</p></Link>
+        </body>
+    )
 }
